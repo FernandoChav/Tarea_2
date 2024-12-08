d3f56969858633f930b21770da4c9c1b3d2c667d
Fernandin <fpatric25@gmail.com>
2024-12-08 12:26:36 -0300

    feat: create project folder structure and configure home route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () => import('../app/Portfolio/Pages/home/home.component').then(m => m.HomeComponent)
+    }
+];
